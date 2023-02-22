da20807c21aa488ac705b1c18a2d43a55205e983
Leonardo Matos <leomp120894@gmail.com>
2023-02-22 20:43:48 -0300

    Update checkout.js
    [skip ci]

--- template/js/custom-js/checkout.js
+++ template/js/custom-js/checkout.js
@@ -1,1 +1,9 @@
 // Add your custom JavaScript for checkout here.
+
+window.EcomSearch.dslMiddlewares.push((dsl) => {
+  dsl.query.bool.filter.push({
+    terms: {
+      'categories.name': ['Ultrabar']
+    }
+  })
+})
