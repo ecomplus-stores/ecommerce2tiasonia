66f72be5e6ab135854fd5b59f018f27b62b8b1a8
Leonardo Matos <leomp120894@gmail.com>
2023-02-22 20:43:36 -0300

    Update pages.js
    [skip ci]

--- template/js/custom-js/pages.js
+++ template/js/custom-js/pages.js
@@ -1,1 +1,9 @@
 // Add your custom JavaScript for storefront pages here.
+
+window.EcomSearch.dslMiddlewares.push((dsl) => {
+  dsl.query.bool.filter.push({
+    terms: {
+      'categories.name': ['Ultrabar']
+    }
+  })
+})
